09d2cff14a3be3d06552e9d7c56fc417393ee542
TSanni <tomas.sanni@gmail.com>
2022-07-30 17:04:31 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Tomas Sanni on 7/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
